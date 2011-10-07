8f3347cb7c944f36638cd9f0e58963865f610312
Kristian Haugene <kristian.haugene@capgemini.com>
2011-10-07 16:53:21 +0200

    first commit

---
+++ src/test/scala/sample/junit.scala
@@ -0,0 +1,17 @@
+package sample
+
+import org.junit._
+import Assert._
+
+@Test
+class AppTest {
+
+    @Test
+    def testOK() = assertTrue(true)
+    
+//    @Test
+//    def testKO() = assertTrue(false)
+
+}
+
+
